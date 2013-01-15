6ed8bb2ce5e79ea41640e440a79551d4aa2e3254
Ryan Ramage <ryan.ramage@gmail.com>
2013-01-14 18:50:05 -0700

    Updated

---
+++ update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl   http://reupholster.iriscouch.com/dashboard/_design/dashboard/static/dashboard.couch -o assets/dashboard.couch
+curl   https://github.com/garden20/garden-futon/archive/master.zip -o assets/futon.zip
